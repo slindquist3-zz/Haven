07aebcf85f18ed4547fb399bc9aae8ac3648e64e
slindquist3 <slindquist3@gmail.com>
2017-02-22 12:11:33 -0500

    Changed origins

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://git.heroku.com/haven-rails-api.git'
+    origins '*'
 
     resource '*',
       headers: :any,
