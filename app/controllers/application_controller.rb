a9b896a52390157207634a6bd3ae095fa6a903c9
slindquist3 <slindquist3@gmail.com>
2017-02-21 23:31:47 -0500

    added more access-control...

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,26 @@
 class ApplicationController < ActionController::API
  before_action :authenticate_user
- before_filter :set_access
+ before_filter :cors_preflight_check
+ after_filter :cors_set_access_control_headers
 
+ def cors_set_access_control_headers
+   headers['Access-Control-Allow-Origin'] = '*'
+   headers['Access-Control-Allow-Methods'] = 'POST, GET, PUT, DELETE, OPTIONS'
+   headers['Access-Control-Allow-Headers'] = 'Origin, Content-Type, Accept, Authorization, Token'
+   headers['Access-Control-Max-Age'] = "1728000"
+ end
+
+ def cors_preflight_check
+   if request.method == 'OPTIONS'
+     headers['Access-Control-Allow-Origin'] = '*'
+     headers['Access-Control-Allow-Methods'] = 'POST, GET, PUT, DELETE, OPTIONS'
+     headers['Access-Control-Allow-Headers'] = 'X-Requested-With, X-Prototype-Version, Token'
+     headers['Access-Control-Max-Age'] = '1728000'
+
+     render :text => '', :content_type => 'text/plain'
+   end
+ end
+ 
  def authenticate_user
    render json: {error: "unauthorized user!!!"} unless signed_in?
  end
@@ -14,8 +33,5 @@ class ApplicationController < ActionController::API
    User.find(Auth.decode(request.env["HTTP_AUTHORIZATION"])["user_id"]) if request.env["HTTP_AUTHORIZATION"].present?
  end
 
-  def set_access
-    @response.headers["Access-Control-Allow-Origin"] = "*"
-  end
 
 end
